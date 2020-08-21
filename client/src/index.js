07e0f3921c75932e3a0e307c263c2b0bb00c1b76
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-08-21 16:22:19 +0200

    :tada: changed to HashRouter

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./pages/App";
 import "./styles/index.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import AuthRoute from "./components/AuthRoute";
 import { Provider } from "react-redux";
 import store from "./utils/store";
