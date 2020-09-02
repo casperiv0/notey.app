4b0bef5e2801443d716eac0294cc9d1a6ca09e29
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-09-02 19:25:43 +0200

    :tada: Added signup

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,8 +1,9 @@
 import { Router, Response } from "express";
 import { IRequest } from "../types";
-import User from "../models/User.model";
-import { compareSync } from "bcrypt";
+import User, { IUser } from "../models/User.model";
+import { compareSync, hashSync } from "bcrypt";
 import { useToken } from "../hooks";
+import { AuthUser } from "../interfaces";
 const router: Router = Router();
 
 router.post("/signin", async (req: IRequest, res: Response) => {
@@ -14,9 +15,7 @@ router.post("/signin", async (req: IRequest, res: Response) => {
   }
 
   if (username && password) {
-    const user: any = await User.findOne({ username }).catch((e) =>
-      console.log(e)
-    );
+    const user = await User.findOne({ username });
 
     if (!user) {
       return res.json({
@@ -34,7 +33,7 @@ router.post("/signin", async (req: IRequest, res: Response) => {
       });
     }
 
-    const data = {
+    const data: AuthUser = {
       _id: user._id,
       username: user.username,
     };
@@ -55,6 +54,58 @@ router.post("/signin", async (req: IRequest, res: Response) => {
       error: "Please fill in all fields",
       status: "error",
     });
+  }
+});
+
+router.post("/signup", async (req: IRequest, res: Response) => {
+  const { username, password, password2 } = req.body;
+
+  if (username && password && password2) {
+    if (password !== password2) {
+      return res.json({
+        error: "passwords do not match",
+        status: "error",
+      });
+    }
+
+    const user: any = await User.findOne({ username: username });
+
+    if (user) {
+      return res.json({ error: "username is already in use", status: "error" });
+    }
+
+    const hash = hashSync(password, 15);
+
+    const newUser: IUser = new User({ username, password: hash });
+
+    try {
+      await newUser.save();
+    } catch (e) {
+      console.log(e);
+      return res.json({
+        error: "Something went wrong signin up",
+        status: "error",
+      });
+    }
+
+    const data: AuthUser = {
+      _id: newUser._id,
+      username: newUser.username,
+    };
+
+    const token = useToken(data, 3600000);
+
+    res.cookie("__token", token, {
+      httpOnly: true,
+      expires: new Date(Date.now() + 3600000),
+    });
+
+    return res.json({
+      user: data,
+      status: "success",
+    });
+  } else {
+    return res.json({ error: "Please fill in all fields", status: "error" });
   }
 });
 
