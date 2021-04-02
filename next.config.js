e5feee3baf890b760317b5301f637e43aa21342e
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2021-04-02 10:27:50 +0200

    :hammer: chore: add redirects

--- next.config.js
+++ next.config.js
@@ -2,4 +2,18 @@ module.exports = {
   future: {
     webpack5: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/signup",
+        destination: "/auth/register",
+        permanent: true,
+      },
+      {
+        source: "/signin",
+        destination: "/auth/login",
+        permanent: true,
+      },
+    ];
+  },
 };
