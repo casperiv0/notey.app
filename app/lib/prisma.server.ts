188639bf440f992477d0ccb39195af3bc288a927
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2021-11-22 17:27:18 +0100

    :hammer: minor change

--- app/lib/prisma.server.ts
+++ app/lib/prisma.server.ts
@@ -1,6 +1,26 @@
+/**
+ * thanks to https://github.com/remix-run/remix-jokes/blob/main/app/utils/db.server.ts
+ */
 import { PrismaClient } from "@prisma/client";
 
-const prismaClient = new PrismaClient({ errorFormat: "pretty", log: ["error", "info", "warn"] });
+let prisma: PrismaClient;
 
-// @ts-expect-error ignore
-export const prisma: typeof prismaClient = (global.prisma ??= prismaClient);
+declare global {
+  var __db: PrismaClient | undefined;
+}
+
+// this is needed because in development we don't want to restart
+// the server with every change, but we want to make sure we don't
+// create a new connection to the DB with every change either.
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+  prisma.$connect();
+} else {
+  if (!global.__db) {
+    global.__db = new PrismaClient();
+    global.__db.$connect();
+  }
+  prisma = global.__db;
+}
+
+export { prisma  };
