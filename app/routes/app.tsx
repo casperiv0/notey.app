1dde301d737fefa813fa15d7a45d17729f98daff
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2021-11-26 17:24:59 +0100

    :hammer: remove unused class

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -61,9 +61,7 @@ export default function App() {
 
   return (
     <Layout>
-      <div className="responsive-app">
-        <Outlet />
-      </div>
+      <Outlet />
 
       <CreditsModal />
     </Layout>
