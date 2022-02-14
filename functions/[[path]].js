123a1a22d40c9f4b1a1eb12cb720eca56a3acf3f
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-02-14 12:10:31 +0100

    :tada: feat: try out cloudflare pages

---
+++ functions/[[path]].js
@@ -0,0 +1,12 @@
+import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
+
+// @ts-expect-error ignore
+import * as build from "../build";
+
+const handleRequest = createPagesFunctionHandler({
+  build,
+});
+
+export function onRequest(context) {
+  return handleRequest(context);
+}
--- remix.env.d.ts
+++ remix.env.d.ts
@@ -1,2 +1,4 @@
 /// <reference types="@remix-run/dev" />
 /// <reference types="@remix-run/node/globals" />
+/// <reference types="@remix-run/cloudflare-pages/globals" />
+/// <reference types="@cloudflare/workers-types" />
