43af620a71e0161ba3b3324ed9c7ef5519d773f4
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-03-29 17:43:25 +0200

    :hammer: fix: set correct width for Editor

--- app/components/editor/Editor.tsx
+++ app/components/editor/Editor.tsx
@@ -40,7 +40,7 @@ export const Editor = ({ overwrite }: Props) => {
     />
   ) : (
     <div
-      style={{ height: "calc(100vh - 3.55rem)", overflowY: "auto" }}
+      style={{ height: "calc(100vh - 3.55rem)", overflowY: "auto", width: "calc(100vw - 300px)" }}
       className="w-full px-4 py-2 text-lg bg-gray-100 dark:bg-dark preview-styles"
       id="note-preview-area"
       dangerouslySetInnerHTML={{ __html: overwrite?.note?.markdown ?? (note.markdown as string) }}
