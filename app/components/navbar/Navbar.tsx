121176a7ee9f3a979041bc43646c1f049246e113
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-05-22 11:15:25 +0200

    :hammer: chore: bump deps

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -94,7 +94,7 @@ export const Navbar = () => {
 
   return (
     <header className="relative w-full h-14">
-      <nav className="absolute top-0 left-0 flex items-center justify-between w-full p-4 h-14 border-b-[1.5px] border-l-[1.5px] border-gray-300 dark:border-dark-5 bg-gray-200 dark:bg-dark">
+      <nav className="absolute top-0 left-0 flex items-center justify-between w-full p-4 h-14 border-b-[1.5px] border-gray-300 dark:border-dark-5 bg-gray-200 dark:bg-dark">
         <div className="flex items-center gap-3">
           <Button
             onClick={() => toggleSidebar()}
