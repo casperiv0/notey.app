db0dc351863eac60b021173612489a6033ac7f65
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-08-01 15:39:48 +0200

    :bug: fix: breaking radix changes

--- app/components/Tooltip.tsx
+++ app/components/Tooltip.tsx
@@ -1,20 +1,21 @@
 import * as RTooltip from "@radix-ui/react-tooltip";
 
 interface Props {
-  children: React.ReactChild;
+  children: React.ReactNode;
   trigger: any;
 }
 
 export const Tooltip = ({ trigger: Trigger, children }: Props) => {
   return (
-    <RTooltip.Root delayDuration={50}>
-      <RTooltip.Trigger asChild>
-        <span>{Trigger}</span>
-      </RTooltip.Trigger>
-
-      <RTooltip.Content className="p-1.5 px-3 rounded-md shadow-md bg-gray-300 dark:bg-dark-3 tooltipOpen max-w-[300px] font-medium">
-        {children}
-      </RTooltip.Content>
-    </RTooltip.Root>
+    <RTooltip.Provider delayDuration={50}>
+      <RTooltip.Root>
+        <RTooltip.Trigger asChild>
+          <span>{Trigger}</span>
+        </RTooltip.Trigger>
+        <RTooltip.Content className="p-1.5 px-3 rounded-md shadow-md bg-gray-300 dark:bg-dark-3 tooltipOpen max-w-[300px] font-medium">
+          {children}
+        </RTooltip.Content>
+      </RTooltip.Root>
+    </RTooltip.Provider>
   );
 };
